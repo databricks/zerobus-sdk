71683cace8dc9cce1ad56838be863daa5db266b7
Teodor Delibasic <teodor.delibasic@databricks.com>
2026-02-04 10:53:59 +0100

    [JNI] Remove MacOS from workflow and rename native methods (#86)
    * Remove MacOS from workflow
    
    Signed-off-by: teodordelibasic-db <teodor.delibasic@databricks.com>
    
    * Rename native methods for base stream class
    
    Signed-off-by: teodordelibasic-db <teodor.delibasic@databricks.com>
    
    * Fix fmt
    
    Signed-off-by: teodordelibasic-db <teodor.delibasic@databricks.com>
    
    * Spawn blocking threads for futures
    
    Signed-off-by: teodordelibasic-db <teodor.delibasic@databricks.com>
    
    * Fix fmt
    
    Signed-off-by: teodordelibasic-db <teodor.delibasic@databricks.com>
    
    * Fix lint
    
    Signed-off-by: teodordelibasic-db <teodor.delibasic@databricks.com>
    
    * Bump JNI version
    
    Signed-off-by: teodordelibasic-db <teodor.delibasic@databricks.com>
    
    ---------
    
    Signed-off-by: teodordelibasic-db <teodor.delibasic@databricks.com>

--- jni/src/async_bridge.rs
+++ jni/src/async_bridge.rs
@@ -63,6 +63,10 @@ pub fn complete_future_exceptionally<'local>(
 /// spawns an async task that will complete the future when the operation finishes.
 /// The result is boxed as a Java Long object.
 ///
+/// The future is completed on Tokio's blocking thread pool (via `spawn_blocking`)
+/// to ensure that any Java callbacks (like `thenApply`) can safely call back into
+/// JNI methods that use `block_on`.
+///
 /// # Type Parameters
 ///
 /// * `F` - The future type that produces the result
@@ -77,63 +81,75 @@ where
     spawn(async move {
         let result = async_fn.await;
 
-        // Attach to the JVM to complete the future
-        let jvm = get_jvm();
-        let mut env = match jvm.attach_current_thread_as_daemon() {
-            Ok(env) => env,
-            Err(e) => {
-                tracing::error!("Failed to attach to JVM: {}", e);
-                return;
-            }
-        };
+        // Complete the future on the blocking thread pool to avoid issues when
+        // Java callbacks call back into JNI methods that use block_on.
+        drop(tokio::task::spawn_blocking(move || {
+            // Attach to the JVM to complete the future
+            let jvm = get_jvm();
+            let mut env = match jvm.attach_current_thread_as_daemon() {
+                Ok(env) => env,
+                Err(e) => {
+                    tracing::error!("Failed to attach to JVM: {}", e);
+                    return;
+                }
+            };
 
-        let future = future_ref.as_obj();
+            let future = future_ref.as_obj();
 
-        match result {
-            Ok(value) => {
-                let handle_value = convert_result(value);
-                // Box as Java Long
-                match env.find_class("java/lang/Long") {
-                    Ok(long_class) => {
-                        match env.new_object(long_class, "(J)V", &[JValue::Long(handle_value)]) {
-                            Ok(java_value) => {
-                                if let Err(e) = complete_future(&mut env, future, java_value) {
-                                    tracing::error!("Failed to complete future: {}", e);
+            match result {
+                Ok(value) => {
+                    let handle_value = convert_result(value);
+                    // Box as Java Long
+                    match env.find_class("java/lang/Long") {
+                        Ok(long_class) => {
+                            match env.new_object(long_class, "(J)V", &[JValue::Long(handle_value)])
+                            {
+                                Ok(java_value) => {
+                                    if let Err(e) = complete_future(&mut env, future, java_value) {
+                                        tracing::error!("Failed to complete future: {}", e);
+                                    }
                                 }
-                            }
-                            Err(e) => {
-                                tracing::error!("Failed to create Long object: {}", e);
-                                if let Some(exc) =
-                                    create_zerobus_exception(&mut env, &e.to_string())
-                                {
-                                    let _ =
-                                        complete_future_exceptionally(&mut env, future, exc.into());
+                                Err(e) => {
+                                    tracing::error!("Failed to create Long object: {}", e);
+                                    if let Some(exc) =
+                                        create_zerobus_exception(&mut env, &e.to_string())
+                                    {
+                                        let _ = complete_future_exceptionally(
+                                            &mut env,
+                                            future,
+                                            exc.into(),
+                                        );
+                                    }
                                 }
                             }
                         }
+                        Err(e) => {
+                            tracing::error!("Failed to find Long class: {}", e);
+                            if let Some(exc) = create_zerobus_exception(&mut env, &e.to_string()) {
+                                let _ = complete_future_exceptionally(&mut env, future, exc.into());
+                            }
+                        }
                     }
-                    Err(e) => {
-                        tracing::error!("Failed to find Long class: {}", e);
-                        if let Some(exc) = create_zerobus_exception(&mut env, &e.to_string()) {
-                            let _ = complete_future_exceptionally(&mut env, future, exc.into());
+                }
+                Err(error) => {
+                    if let Some(exc) = create_exception_from_error(&mut env, &error) {
+                        if let Err(e) = complete_future_exceptionally(&mut env, future, exc.into())
+                        {
+                            tracing::error!("Failed to complete future exceptionally: {}", e);
                         }
                     }
                 }
             }
-            Err(error) => {
-                if let Some(exc) = create_exception_from_error(&mut env, &error) {
-                    if let Err(e) = complete_future_exceptionally(&mut env, future, exc.into()) {
-                        tracing::error!("Failed to complete future exceptionally: {}", e);
-                    }
-                }
-            }
-        }
+        }));
     });
 }
 
 /// Spawn an async operation that will complete a Java CompletableFuture<Void>.
 ///
 /// This is a convenience function for operations that don't return a value.
+///
+/// The future is completed on Tokio's blocking thread pool (via `spawn_blocking`)
+/// to ensure that any Java callbacks can safely call back into JNI methods.
 pub fn spawn_and_complete_void<F>(future_ref: GlobalRef, async_fn: F)
 where
     F: Future<Output = Result<(), ZerobusError>> + Send + 'static,
@@ -141,32 +157,37 @@ where
     spawn(async move {
         let result = async_fn.await;
 
-        // Attach to the JVM to complete the future
-        let jvm = get_jvm();
-        let mut env = match jvm.attach_current_thread_as_daemon() {
-            Ok(env) => env,
-            Err(e) => {
-                tracing::error!("Failed to attach to JVM: {}", e);
-                return;
-            }
-        };
-
-        let future = future_ref.as_obj();
-
-        match result {
-            Ok(()) => {
-                if let Err(e) = complete_future_void(&mut env, future) {
-                    tracing::error!("Failed to complete future: {}", e);
+        // Complete the future on the blocking thread pool to avoid issues when
+        // Java callbacks call back into JNI methods that use block_on.
+        drop(tokio::task::spawn_blocking(move || {
+            // Attach to the JVM to complete the future
+            let jvm = get_jvm();
+            let mut env = match jvm.attach_current_thread_as_daemon() {
+                Ok(env) => env,
+                Err(e) => {
+                    tracing::error!("Failed to attach to JVM: {}", e);
+                    return;
                 }
-            }
-            Err(error) => {
-                if let Some(exc) = create_exception_from_error(&mut env, &error) {
-                    if let Err(e) = complete_future_exceptionally(&mut env, future, exc.into()) {
-                        tracing::error!("Failed to complete future exceptionally: {}", e);
+            };
+
+            let future = future_ref.as_obj();
+
+            match result {
+                Ok(()) => {
+                    if let Err(e) = complete_future_void(&mut env, future) {
+                        tracing::error!("Failed to complete future: {}", e);
+                    }
+                }
+                Err(error) => {
+                    if let Some(exc) = create_exception_from_error(&mut env, &error) {
+                        if let Err(e) = complete_future_exceptionally(&mut env, future, exc.into())
+                        {
+                            tracing::error!("Failed to complete future exceptionally: {}", e);
+                        }
                     }
                 }
             }
-        }
+        }));
     });
 }
 
